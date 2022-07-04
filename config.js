9522e604ca5b45333ad4e88c9ceb422e232017f1
ketan0608 <76109939+ketan0608@users.noreply.github.com>
2022-07-04 17:54:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBR7QsBRFd5N6f9iVRQKJy_FLY2CW9r5jc",
+  authDomain: "story-tellling-app-b8fa0.firebaseapp.com",
+  databaseURL: "https://story-tellling-app-b8fa0-default-rtdb.firebaseio.com",
+  projectId: "story-tellling-app-b8fa0",
+  storageBucket: "story-tellling-app-b8fa0.appspot.com",
+  messagingSenderId: "160172699937",
+  appId: "1:160172699937:web:14f87c6eadf488ac4ace60"
+};
